99807f733948c577206fe35b07eb2d8293d4f901
Arter Tendean <arter.tendean.07@gmail.com>
2023-12-12 16:03:01 +0800

    Adding Register API Test

--- accounts/tests/test_accounts_api.py
+++ accounts/tests/test_accounts_api.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APIClient, APITestCase
 
 
 class LoginAPITestCase(APITestCase):
@@ -36,3 +36,42 @@ class LoginAPITestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("There is no user with this email", response.data["email"])
+
+
+class RegisterAPITest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.register_url = reverse("accounts-api:register")
+        self.valid_payload = {
+            "email": "test@example.com",
+            "password": "securepassword123",
+        }
+
+    def test_registration_successful(self):
+        response = self.client.post(
+            self.register_url, self.valid_payload, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_registration_duplicate_email(self):
+        User.objects.create_user(
+            username=self.valid_payload["email"],
+            email=self.valid_payload["email"],
+            password=self.valid_payload["password"],
+        )
+
+        response = self.client.post(
+            self.register_url, self.valid_payload, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("already taken", str(response.data))
+
+    def test_registration_weak_password(self):
+        weak_password_payload = {"email": "test@example.com", "password": "weak"}
+
+        response = self.client.post(
+            self.register_url, weak_password_payload, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("Password must be at least 6 characters", str(response.data))
