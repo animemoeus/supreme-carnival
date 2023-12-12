3aea1f9822e56d6bf81de68790174534d44675b0
Arter Tendean <arter.tendean.07@gmail.com>
2023-12-12 23:43:19 +0800

    Update models

--- countries/models.py
+++ countries/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Country(models.Model):
     name = models.CharField(max_length=25, unique=True)
     flag = models.URLField(max_length=255)
-    currency = models.CharField(max_length=5)
+    currency = models.CharField(max_length=5, unique=True)
 
     class Meta:
         verbose_name = "Country"
