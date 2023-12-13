08b435896be1d4ac6507bcb4b91fccb88252f5d0
Arter Tendean <arter.tendean.07@gmail.com>
2023-12-14 00:02:22 +0800

    Adding `CSRF_TRUSTED_ORIGINS`

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
-    "https://brcg.animemoe.us/accounts/register/",
+    "https://brcg.animemoe.us",
     "https://brcg.fly.dev",
 ]
 
