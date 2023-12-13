02ae90ee6a258e1b411c15bc2253f9b26e029267
Arter Tendean <arter.tendean.07@gmail.com>
2023-12-13 15:58:23 +0800

    Adding categories search API

--- app/urls.py
+++ app/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.api.urls", namespace="accounts-api")),
     path("api/countries/", include("countries.api.urls", namespace="countries-api")),
+    path("api/categories/", include("categories.api.urls", namespace="categories-api")),
     path("accounts/", include("accounts.urls", namespace="accounts")),
     path("countries/", include("countries.urls", namespace="countries")),
     path("categories/", include("categories.urls", namespace="categories")),
---
+++ categories/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import CategoryListAPIView
+
+urlpatterns = [
+    path("", CategoryListAPIView.as_view(), name="index"),
+]
+
+app_name = "categories"
